5b3bb75be76a8954e77933f1dab1049c47cb37ce
velpula-manasa <163998302+velpula-manasa@users.noreply.github.com>
2024-06-20 13:16:40 +0530

    Create style.css

---
+++ style.css
@@ -0,0 +1,37 @@
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+body {
+    height: 100vh;
+    background: linear-gradient(#ffd55c 50%, #5a95ff 50%);
+}
+
+.app {
+    font-family: "Poppins", sans-serif;
+    width: min(95vw, 31.25em);
+    position: absolute;
+    margin: auto;
+    left: 0;
+    right: 0;
+    top: 1.875em;
+}
+
+.container {
+    padding: 2em 2.5em;
+    background-color: #f1f8fb;
+    box-shadow: 0 1em 2em rgba(0, 0, 0, 0.3);
+    border-radius: 0.8em;
+}
+
+#wrapper {
+    position: relative;
+    display: grid;
+    grid-template-columns: 8fr 4fr;
+    gap: 1em;
+}
+
+#wrapper input {
+    w
